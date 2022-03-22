064598162e38747ce94975b8a2d0b68d73186ec7
Bhavani Palla <bhavani.palla@gmail.com>
2022-03-22 07:55:00 +0100

    Updated

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const cors = require('cors')
+const app = express();
+const cors = require("cors");
 
 //Port number
-const port = 3100
-app.use(express.json())
-app.use(cors())
+const port = 3100;
+app.use(express.json());
+app.use(cors());
 
-const apiRouter = require('./router/api')
+const apiRouter = require("./router/api");
 
-app.use('/api', apiRouter)
-
+app.use("/api", apiRouter);
 
 //Create server listner
 app.listen(port, () => {
-    console.log('Server started at port ', port)
-})
-
+  console.log("Server started at port : ", port);
+});
